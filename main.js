baba63d5b4c28ddbe262cc6a2acb34f4afc0f097
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-07-10 13:13:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -29,12 +29,30 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+
+    },5000);
 }
 
 camera = document.getElementById("camera");
 Webcam.set({
-    width:350,
+    width:450,
     height:350,
     image_format:'jpeg',
-    jpeg_quality:90
+    jpeg_quality:99
 });
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
